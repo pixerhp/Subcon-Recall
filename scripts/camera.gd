d9674231a074dc0fd0a8e52c9d6915770045cc58
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2026-01-03 19:53:33 -0500

    Revise imputmap movement directions

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -2,7 +2,7 @@ extends Camera2D
 
 var move_normalized: Vector2 = Vector2()
 func _process(_delta):
-	move_normalized = Input.get_vector("left", "right", "up", "down")
+	move_normalized = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	move_normalized = move_normalized.normalized()
 	
 	self.position += move_normalized
