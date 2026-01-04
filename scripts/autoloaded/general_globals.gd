532597529ff10f8414c1b8b575834b4b79f236e1
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2026-01-03 19:50:43 -0500

    Implement fullscreen toggle and quit

---
+++ scripts/autoloaded/general_globals.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func _process(_delta):
+	# Process inputs that should work regardless of game state:
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("fullscreen_toggle"):
+		if not DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
